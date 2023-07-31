3073ae344f16e2c21bcbe031647275aa4217df1a
NovikovaVolha <olga_klyuka@mail.ru>
2023-07-31 16:57:30 +0300

    feat: add validation

---
+++ src/main/java/com/itbootcamp/test_task/dto/UserDto.java
@@ -0,0 +1,36 @@
+package com.itbootcamp.test_task.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.itbootcamp.test_task.dto.enums.RoleDto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    @JsonIgnore
+    private Integer id;
+    @NotBlank
+    @Pattern(regexp = "^[a-zA-Z]{1,40}$", message = "Must be from 1 to 40 characters.")
+    private String surname;
+    @NotBlank
+    @Pattern(regexp = "^[a-zA-Z]{1,20}$", message = "Must be from 1 to 20 characters.")
+    private String name;
+    @Pattern(regexp = "^[a-zA-Z]{0,40}$", message = "Must be from 0 to 40 characters.")
+    private String patronymic;
+    @Pattern(regexp = "^([_a-zA-Z0-9-]+(\\.[_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9]+)*(\\.[a-zA-Z]{2,})){0,50}$",
+            message = "Must be from 0 to 50 characters. Several special characters are allowed, " +
+                      "including period (.), hyphen (-), underscore (_) and @ sign.")
+    private String email;
+    private List<RoleDto> roles;
+
+}
